01aa86efb49525904a15184cef7456463a75a420
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-08-07 12:28:59 -0700

    Switch to using test/*_test.rb over spec/*_spec.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,6 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/pride'
 
+# Add "require_relative"s here.
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
